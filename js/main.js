50df51d42f79c883991705089b5177521c90aa4d
Bulovask <ageuassuncaosimoesgalindo@gmail.com>
2022-05-16 14:09:37 -0300

    salvamento automatico no localStorage

--- js/main.js
+++ js/main.js
@@ -15,6 +15,9 @@ let init = 0;
 let ms = 0;
 let pauseMs = 0;
 
+loadFromLocalStorage();
+if(run) start();
+
 //funções do cronômetro
 function start() {
 	if(!run) {
@@ -25,6 +28,7 @@ function start() {
 	else {
 		ms = Date.now() - init + pauseMs;
 	}
+	saveInLocalStorage();
 	update();
 	cancelAnimationFrame(reqAniFra);
 	reqAniFra = requestAnimationFrame(start);
@@ -33,17 +37,38 @@ function start() {
 function pause() {
 	cancelAnimationFrame(reqAniFra);
 	run = false;
+	saveInLocalStorage();
 }
 
 function reset() {
 	ms = 0;
-	stop();
+	pause();
+	saveInLocalStorage();
 	update();
+}
+
+function saveInLocalStorage() {
+    localStorage.setItem("savedRun", run);
+    localStorage.setItem("savedInit", init);
+    localStorage.setItem("savedMs", ms);
+    localStorage.setItem("savedPauseMs", pauseMs);
+}
+
+function loadFromLocalStorage() {
+    const savedRun = localStorage.getItem("savedRun");
+    const savedInit = localStorage.getItem("savedInit");
+    const savedMs = localStorage.getItem("savedMs");
+    const savedPauseMs = localStorage.getItem("savedPauseMs");
+    
+    savedRun == "true" ? run = true : run = false;
+    Number(savedInit) ? init = Number(savedInit) : 0;
+    Number(savedMs) ? ms = Number(savedMs) : 0;
+    Number(savedPauseMs) ? pausedMs = Number(savedPauseMs) : 0;
 }
 
 //atualizações da tela
 function update() {
-	displayMain.innerText = textDisplay(ms);
+	displayMain.innerHTML = textDisplay(ms);
 	resizeFontSize();
 }
 
@@ -71,20 +96,20 @@ function textDisplay(ms) {
 	
 	//Decidindo forma de exibição
 	if(ms < 60000) {
-		return s + 's.' + mspor10;
+		return s + '<small>s</small>.' + mspor10;
 	}
 	else if(ms < 60 * 60000) {
-		return min + 'min:' + s + 's.' + mspor10;
+		return min + '<small>min</small>:' + s + '<small>s</small>.' + mspor10;
 	}
 	else {
-		return h + 'H:' + min + 'min:' + s + 's.' + mspor10;
+		return h + '<small>H</small>:' + min + '<small>min</small>:' + s + '<small>s</small>.' + mspor10;
 	}
 }
 
 function resizeFontSize() {
     const boxWidth = document.querySelector("#display").clientWidth;
-    const length = displayMain.innerText.length;
-    const fontSize =  `${boxWidth / length * 1.6}px`;
+    const length = displayMain.textContent.length;
+    const fontSize =  `${boxWidth / length * 1.75}px`;
     displayMain.style.setProperty("--font-size", fontSize);
 }
 
